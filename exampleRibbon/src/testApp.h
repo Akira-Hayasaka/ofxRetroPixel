6d5ec1a0197de0ea6a525b50d9ac69c22aa02e0f
Jeff Crouse <jefftimesten@gmail.com>
2012-11-29 12:15:56 -0500

    update project

--- exampleRibbon/src/testApp.h
+++ exampleRibbon/src/testApp.h
@@ -1,38 +1,27 @@
-/**
- *
- * OFDevCon Example Code Sprint
- * Camera Ribbon example
- * This example generates ribbons along the mouse trail that descend in space
- * When you click space bar, you can
- *
- * Created by James George for openFrameworks workshop at Waves Festival Vienna sponsored by Lichterloh and Pratersauna
- * Adapted during ofDevCon on 2/23/2012
- */
-
 #pragma once
 
 #include "ofMain.h"
 #include "ofxRetroPixel.h"
 
+
 class testApp : public ofBaseApp{
 
-public:
-    
-    void setup();
-    void update();
-    void draw();
+	public:
+		void setup();
+		void update();
+		void draw();
 
-    void keyPressed  (int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-	
-	//this holds all of our points
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+    //this holds all of our points
     vector<ofVec3f> points;
     //this keeps track of the center of all the points
     ofVec3f center;
@@ -42,10 +31,10 @@ public:
 	
 	//if usecamera is true, we'll turn on the camera view
     bool usecamera;
-    
+    int idx;
     
 private:
     
-    ofxRetroPixel retro; 
-    ofFbo screen;    
+    ofxRetroPixel retro;
+    ofFbo screen;
 };
